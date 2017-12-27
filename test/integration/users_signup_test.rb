00bf9a3c62cba89c6ee00a039979d7613a4d0a8f
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2017-12-27 18:50:39 +0200

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -20,4 +20,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       assert_select '#user_password_confirmation'
       assert_select 'form[action="/signup"]'
     end
+
+    test 'valid signup information' do
+      get signup_path
+      assert_difference 'User.count', 1 do
+        post users_path, params: { user: { name:  'Example User',
+                                           email: 'user@example.com',
+                                           password:              'password',
+                                           password_confirmation: 'password' } }
+      end
+      follow_redirect!
+      assert_template 'users/show'
+    end
 end
