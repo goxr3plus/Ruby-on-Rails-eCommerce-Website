5c18b1e7f8def9601f1e6a08b5360ab8bf54ecc6
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2017-12-27 14:11:12 +0200

    User Authentication added

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171226153536) do
+ActiveRecord::Schema.define(version: 20171227115456) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
