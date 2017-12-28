d95110f34b0499cbceab0f2f77069af914d64e6f
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2017-12-28 18:13:40 +0200

    Added Remember Me on Login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       log_in user
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       # Create an error message.
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,4 +16,18 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+   # Log in as a particular user.
+   def log_in_as(user)
+     session[:user_id] = user.id
+   end
+
+ class ActionDispatch::IntegrationTest
+
+   # Log in as a particular user.
+   def log_in_as(user, password: 'password', remember_me: '1')
+     post login_path, params: { session: { email: user.email,
+                                           password: password,
+                                           remember_me: remember_me } }
+   end
 end
