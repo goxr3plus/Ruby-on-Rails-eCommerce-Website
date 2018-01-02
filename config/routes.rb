c10ccbb23c15854642f5c6a1f3ac296dab18b43d
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2018-01-02 19:16:29 +0200

    Initialize Password Resets

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,20 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root 'static_pages#home'
   get  '/help', to: 'static_pages#help', as: 'helf'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup', to: 'users#new'
-  post  '/signup', to: 'users#create'
+  post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: %i[new create edit update]
 end
