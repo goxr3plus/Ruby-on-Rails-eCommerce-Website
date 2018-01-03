f2c70d683a52cd881e137c3f3c3985a21c2548e3
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2018-01-03 15:39:11 +0200

    Initialized User Microposts

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@
 
 Rails.application.routes.draw do
   get 'password_resets/new'
-
   get 'password_resets/edit'
-
   root 'static_pages#home'
   get  '/help', to: 'static_pages#help', as: 'helf'
   get  '/about',   to: 'static_pages#about'
@@ -15,6 +13,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: %i[new create edit update]
