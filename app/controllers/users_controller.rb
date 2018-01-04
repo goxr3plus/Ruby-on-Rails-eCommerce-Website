d5c8d88ec2bc684d368636513b49465149cd801b
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2018-01-04 19:34:00 +0200

    Added Bootstrap glyph icons and modified home page\

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,7 +53,7 @@ class UsersController < ApplicationController
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = 'User deleted'
-    redirect_to users_url
+    redirect_back(fallback_location: users_url)
   end
 
   private
