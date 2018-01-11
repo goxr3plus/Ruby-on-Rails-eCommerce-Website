f0a387731cff92fc2f6ea209b47e6c2f49e33eab
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2018-01-11 20:24:09 +0200

    Temporarily disabled nice sccroll of JQUery

--- app/assets/javascripts/nicescrollFix.js
+++ app/assets/javascripts/nicescrollFix.js
@@ -1,12 +1,15 @@
-$(function () {
-  $("body").niceScroll({
-    cursorwidth: 12,
-    cursoropacitymin: 0.4,
-    cursorcolor: 'firebrick',
-    cursorborder: 'none',
-    cursorborderradius: 4,
-    autohidemode: 'leave'
-  }); // free your immagination
-  $("body").niceScroll().resize();
-  // console.log("Entered..");
-});
+// <!-- Add the custom scroll bar -->
+// <!-- <script >
+//   $(function () {
+//     $("body").niceScroll({
+//       cursorwidth: 12,
+//       cursoropacitymin: 0.4,
+//       cursorcolor: 'firebrick',
+//       cursorborder: 'none',
+//       cursorborderradius: 4,
+//       autohidemode: 'leave'
+//     }); // free your immagination
+//     $("body").niceScroll().resize();
+//     // console.log("Entered..");
+//   });
+// </script> -->
