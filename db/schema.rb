7da4c7b7150a7533a87560a182db5718336ce675
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2018-01-15 14:27:31 +0200

    Added RelationShips Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180114120447) do
+ActiveRecord::Schema.define(version: 20180115122408) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -52,6 +52,16 @@ ActiveRecord::Schema.define(version: 20180114120447) do
     t.datetime "updated_at", null: false
     t.decimal "price", default: "0.0"
     t.string "picture"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
