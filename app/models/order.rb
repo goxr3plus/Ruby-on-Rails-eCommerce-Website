d0d68ca717d206fea358e350dd3d229b88509994
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2018-02-01 18:45:03 +0200

    Remade the Cart User Interface

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,13 @@
 class Order < ApplicationRecord
   has_many :line_items, dependent: :destroy
+  # enum pay_method: {
+  #   'Check' => 0,
+  #   'Credit card' => 1,
+  #   'Purchase order' => 2
+  # }
   belongs_to :user
+  # validates :pay_method, :description, presence: true
+  # validates :pay_method, inclusion: pay_methods.keys
 
   # LOGIC
   def sub_total
