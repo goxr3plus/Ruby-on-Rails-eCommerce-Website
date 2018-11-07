aa3e9e63b7c4dec1e9a2325403bf62259094c323
Tanmaya-cybrilla <tanmay2017@outlook.in>
2018-11-08 03:03:57 +0530

    Fix typo in seed file and optimize query

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,7 +38,7 @@ product2 = Product.create({:name=>"milk", :price => 3})
 product3 = Product.create({:name=>"bread", :price => 5.50})
 product4 = Product.create({:name=>"bacon", :price => 10})
 product5 = Product.create({:name=>"cheese", :price => 3.20})
-product5 = Product.create({:name=>"molohia", :price => 15.20})
+product6 = Product.create({:name=>"molohia", :price => 15.20})
 
 puts "Total number of products: #{Product.all.count}"
 puts "Product names: #{Product.all.pluck("name")}"
@@ -47,14 +47,14 @@ puts "Product2: #{product2.name} price: #{product2.price.round(2)}"
 puts "Product3: #{product3.name} price: #{product3.price.round(2)}"
 puts "Product4: #{product4.name} price: #{product4.price.round(2)}"
 puts "Product5: #{product5.name} price: #{product5.price.round(2)}"
-puts "Product5: #{product5.name} price: #{product6.price.round(2)}"
+puts "Product6: #{product6.name} price: #{product6.price.round(2)}"
 
 # CART
 Cart.destroy_all
 puts "\nTotal cart count: #{Cart.all.count}"
 
 # Following relationships
-users = User.all
+users = User.first(50)
 user  = users.first
 following = users[2..50]
 followers = users[3..40]
