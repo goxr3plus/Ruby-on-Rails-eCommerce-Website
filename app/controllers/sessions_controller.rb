f8153a147cc04b5a197111377eb036245520b33b
Tanmaya-cybrilla <tanmay2017@outlook.in>
2018-11-08 12:51:12 +0530

    Rubocop self corrected automatic errors on controllers - Refactored products controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,6 @@ class SessionsController < ApplicationController
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
 
-        
         # redirect_back_or user
         redirect_back_or root_url
       else
