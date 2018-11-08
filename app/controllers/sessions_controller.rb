0928d306cc65e0b345cf66e382b275e4f32a05b8
Tanmaya-cybrilla <tanmay2017@outlook.in>
2018-11-09 01:03:01 +0530

    Fix rubocop errors for sessions controller and refactor code - Create new methods for checking authentication of user and perform action when authenticated - Fix intedation errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,30 +1,19 @@
 class SessionsController < ApplicationController
   # GET
   def new
-     # byebug
-   end
+    # byebug
+  end
 
   # POST
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      if user.activated?
-        log_in user
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-
-        # redirect_back_or user
-        redirect_back_or root_url
-      else
-        message  = 'Account not activated. '
-        message += 'Check your email for the activation link.'
-        flash[:warning] = message
-        redirect_to root_url
-      end
+    if authenticated?(user)
+      perform_authenticated_action(user)
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
-    end
+  end
 
   def destroy
     log_out if logged_in?
@@ -33,4 +22,25 @@ class SessionsController < ApplicationController
 
   # Destroy
   def logout; end
+
+  private
+
+  def authenticated?(user)
+    user && user.authenticate(params[:session][:password])
+  end
+
+  def perform_authenticated_action(user)
+    if user.activated?
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+
+      # redirect_back_or user
+      redirect_back_or root_url
+    else
+      message = 'Account not activated. '
+      message += 'Check your email for the activation link.'
+      flash[:warning] = message
+      redirect_to root_url
+    end
+  end
 end
