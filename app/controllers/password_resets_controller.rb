8d5fc1d587c7448c2e66a9fe8a50a975c3c445c1
Tanmaya-cybrilla <tanmay2017@outlook.in>
2018-11-09 01:43:30 +0530

    Fix rubocop errors for password reset controller - Refactor code in create and update method containing multiple lines - Rename get_user to fetch_user

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,11 +1,12 @@
 # Cases
 # 1.An expired password reset
 # 2.A failed update due to an invalid password
-# 3.A failed update (which initially looks “successful”) due to an empty password and confirmation
+# 3.A failed update (which initially looks 'successful') due to an empty
+# password and confirmation
 # 4.A successful update
 
 class PasswordResetsController < ApplicationController
-  before_action :get_user,         only: %i[edit update]
+  before_action :fetch_user, only: %i[edit update]
   before_action :valid_user,       only: %i[edit update]
   before_action :check_expiration, only: %i[edit update] # Case (1)
 
@@ -14,11 +15,7 @@ class PasswordResetsController < ApplicationController
   def create
     @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
-      @user.create_reset_digest
-      @user.send_password_reset_email
-      # flash[:info] = 'Email sent with password reset instructions'
-      flash[:info] = "Visit this link to reset your account ~-> #{edit_password_reset_url(@user.reset_token, email: @user.email)}".html_safe
-      redirect_to root_url
+      reset_user_password
     else
       flash.now[:danger] = 'Email address not found'
       render 'new'
@@ -32,10 +29,7 @@ class PasswordResetsController < ApplicationController
       @user.errors.add(:password, "can't be empty")
       render 'edit'
     elsif @user.update_attributes(user_params)          # Case (4)
-      log_in @user
-      @user.update_attribute(:reset_digest, nil)
-      flash[:success] = 'Password has been reset.'
-      redirect_to @user
+      redirect_with_success_message
     else
       render 'edit'                                     # Case (2)
     end
@@ -49,7 +43,7 @@ class PasswordResetsController < ApplicationController
 
   # Before filters
 
-  def get_user
+  def fetch_user
     @user = User.find_by(email: params[:email])
   end
 
@@ -63,9 +57,27 @@ class PasswordResetsController < ApplicationController
 
   # Checks expiration of reset token.
   def check_expiration
-    if @user.password_reset_expired?
-      flash[:danger] = 'Password reset has expired.'
-      redirect_to new_password_reset_url
-    end
+    return unless @user.password_reset_expired?
+
+    flash[:danger] = 'Password reset has expired.'
+    redirect_to new_password_reset_url
+  end
+
+  def reset_user_password
+    @user.create_reset_digest
+    @user.send_password_reset_email
+    # flash[:info] = 'Email sent with password reset instructions'
+    reset_password_message = 'Visit this link to reset your account ~-> ' +
+                             edit_password_reset_url(@user.reset_token,
+                                                     email: @user.email).to_s
+    flash[:info] = reset_password_message.html_safe
+    redirect_to root_url
+  end
+
+  def redirect_with_success_message
+    log_in @user
+    @user.update_attribute(:reset_digest, nil)
+    flash[:success] = 'Password has been reset.'
+    redirect_to @user
   end
 end
