b96544a4f10e913d22df884b5ee780d2c80d97c2
Tanmaya-cybrilla <tanmay2017@outlook.in>
2018-11-09 14:25:04 +0530

    Fix rubocop errors for account activation controller and refactor the code

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,14 +1,24 @@
 class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by(email: params[:email])
-    if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      user.activate
-      log_in user
+    if valid_for_activation?(user)
+      activate_account(user)
       flash[:success] = 'Account activated!'
       redirect_to user
     else
       flash[:danger] = 'Invalid activation link'
       redirect_to root_url
     end
-    end
+  end
+
+  private
+
+  def valid_for_activation?(user)
+    user && !user.activated? && user.authenticated?(:activation, params[:id])
+  end
+
+  def activate_account(user)
+    user.activate
+    log_in user
+  end
 end
