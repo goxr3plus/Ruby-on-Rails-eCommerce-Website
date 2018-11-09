1f8c932d4eace799cb296267ef9e47c0999f58da
Tanmaya-cybrilla <tanmay2017@outlook.in>
2018-11-09 15:11:27 +0530

    Fix rubocop erros for application controller and refactor code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,32 +9,27 @@ class ApplicationController < ActionController::Base
   def set_cart
     if session[:cart_id]
       cart = Cart.find_by(id: session[:cart_id])
-      if cart.present?
-        @current_cart = cart
-      else
-        session[:cart_id] = nil
-      end
+      cart.present? ? (@current_cart = cart) : (session[:cart_id] = nil)
     end
+    return unless session[:cart_id].nil?
 
-    if session[:cart_id].nil?
-      @current_cart = Cart.create(user_id: nil)
-      session[:cart_id] = @current_cart.id
-    end
+    @current_cart = Cart.create(user_id: nil)
+    session[:cart_id] = @current_cart.id
   end
 
   # Confirms a logged-in user.
   def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = 'Please log in.'
-      redirect_to login_url
-    end
+    return if logged_in?
+
+    store_location
+    flash[:danger] = 'Please log in.'
+    redirect_to login_url
   end
 
   def user_is_admin
-    unless logged_in? && current_user.admin?
-      flash[:info] = "You don't have the privilages for this action!"
-      redirect_back(fallback_location: root_url)
-    end
+    return if logged_in? && current_user.admin?
+
+    flash[:info] = "You don't have the privilages for this action!"
+    redirect_back(fallback_location: root_url)
   end
 end
