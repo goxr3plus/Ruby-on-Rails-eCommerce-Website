bc9a2233010104e8a85ec5dc880d9a566f8494c4
Tanmaya-cybrilla <tanmay2017@outlook.in>
2018-11-11 00:36:37 +0530

    Add factory bot gem and configuration for it

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ group :test do
   gem 'guard',                    '2.13.0'
   gem 'guard-minitest',           '2.4.4'
   gem 'minitest'  ,               '5.10.3'
+  gem 'factory_bot'
 end
 
 group :production do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,11 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include FactoryBot::Syntax::Methods
+
+  config.before do
+    FactoryBot.find_definitions
+  end
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
